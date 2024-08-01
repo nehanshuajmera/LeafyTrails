b272c171b45eeb12100628dd3c2d04f4732ed4e9
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-02 04:53:07 +0530

    Created Header Component and added some Styles to it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Header } from './components/header/header.component'
 
 const App = () => {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Header />
+      <div className="pages">
+        <Routes>
+
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ client/src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import './header.styles.scss';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-box">
+        <img src="https://cdn-icons-png.flaticon.com/512/7893/7893979.png" alt="Logo" className="logo" />
+      </div>
+      
+    </header>
+  )
+}
