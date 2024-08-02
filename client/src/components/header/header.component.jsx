15936d6cc929a5771aa5a656eecead0271ad1f58
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-03 02:29:17 +0530

    Best practice BEM (Block Element Modifier) model is used to name the classname in css

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 export const Header = () => {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src="https://cdn-icons-png.flaticon.com/512/7893/7893979.png" alt="Logo" className="logo" />
+      <div className="header__logo-box">
+        <img src="https://cdn-icons-png.flaticon.com/512/7893/7893979.png" alt="Logo" className="header__logo" />
       </div>
-      <div className="hero-text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
         </h1>
-        <Link to="#" className='btn btn-white btn-animated'>Discover our tours</Link>
+        <Link to="#" className='btn btn--white btn--animated'>Discover our tours</Link>
       </div>
     </header>
   )
