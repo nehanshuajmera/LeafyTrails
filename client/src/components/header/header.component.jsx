543a1dfd5d46e26af15d9f58330219cf9c393741
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-02 16:08:55 +0530

    Animation added to the primary heading

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -6,7 +6,12 @@ export const Header = () => {
       <div className="logo-box">
         <img src="https://cdn-icons-png.flaticon.com/512/7893/7893979.png" alt="Logo" className="logo" />
       </div>
-      
+      <div className="hero-text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub">is where life happens</span>
+        </h1>
+      </div>
     </header>
   )
 }
