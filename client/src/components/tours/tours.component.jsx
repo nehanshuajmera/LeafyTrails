f338021349faed9d4f460297f309f9649ca7cab3
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-09 15:46:40 +0530

    Scrollbar styles added

--- client/src/components/tours/tours.component.jsx
+++ client/src/components/tours/tours.component.jsx
@@ -3,7 +3,9 @@ import './tours.styles.scss';
 export const Tours = () => {
   return (
     <div className="tours">
-      
+      <h2 className="heading-secondary">
+        
+      </h2>
     </div>
   )
 }
