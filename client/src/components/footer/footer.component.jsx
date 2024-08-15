3757a0018c796fa974332ede15913e9a3f0cc11c
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-15 20:31:51 +0530

    Footer Almost done, basic styling complete

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -1,51 +1,66 @@
 import { Link } from "react-router-dom";
 import "./footer.styles.scss";
 
+import instagram from "../../assets/instagram.png";
+import facebook from "../../assets/facebook.png";
+import twitter from "../../assets/twitter.png";
+import youtube from "../../assets/youtube.png";
+import gmail from "../../assets/gmail.png";
+
 export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__tagline">
         <h3 className="footer__tagline--main">Ready for an Adventure?</h3>
-        <h3 className="footer__tagline--sub">We're Just a Message Away!</h3>
+        <h3 className="footer__tagline--sub">
+          <Link to="/contact">We're Just a Message Away!</Link>
+        </h3>
       </div>
       <div className="footer__logo-content">
         <div className="footer__logo">
-          <img src="/leafy-travel.png" alt="Footer Logo" />
+          <Link to="/">
+            <img src="/leafy-travel.png" alt="Footer Logo" />
+          </Link>
         </div>
         <div className="footer__links">
           <div className="webpage__links">
             <div className="page-link">
-              <Link to="">Home</Link>
+              <Link to="/">Home</Link>
             </div>
             <div className="page-link">
-              <Link to="">About</Link>
+              <Link to="/about">About</Link>
             </div>
             <div className="page-link">
-              <Link to="">Tours</Link>
+              <Link to="/tours">Tours</Link>
             </div>
             <div className="page-link">
-              <Link to="">Contact Us</Link>
+              <Link to="/contactus">Contact Us</Link>
             </div>
           </div>
           <div className="social-media__links">
             <div className="media-link instagram">
               <Link to="">
-                <i className="fa-brands fa-instagram fa-lg" />
+                <img src={instagram} alt="Instagram" />
               </Link>
             </div>
             <div className="media-link facebook">
               <Link to="">
-                <i className="fa-brands fa-facebook-f fa-lg" />
+                <img src={facebook} alt="Facebook" />
               </Link>
             </div>
             <div className="media-link twitter">
               <Link to="">
-                <i className="fa-brands fa-twitter fa-lg" />
+                <img src={twitter} alt="Twitter" />
               </Link>
             </div>
+            {/* <div className="media-link gmail">
+              <Link to="">
+                <img src={gmail} alt="gmail" />
+              </Link>
+            </div> */}
             <div className="media-link youtube">
               <Link to="">
-                <i className="fa-brands fa-youtube fa-lg" />
+                <img src={youtube} alt="Youtube" />
               </Link>
             </div>
           </div>
