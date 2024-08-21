c0fd917533c1a9bf1d2146361eedc03b0f15d84a
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-21 20:22:37 +0530

    Navigation phase 1 done

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -5,22 +5,24 @@ import { Navigation } from "../navigation/navigation.component";
 
 export const Header = () => {
   return (
-    <header className="header">
+    <>
       <Navigation />
-      <div className="header__logo-box">
-        <Link to="/">
-          <img src="/leafy-travel.png" alt="Logo" className="header__logo" />
-        </Link>
-      </div>
-      <div className="header__text-box">
-        <h1 className="heading__primary">
-          <span className="heading__primary--main">Outdoors</span>
-          <span className="heading__primary--sub">is where life happens</span>
-        </h1>
-        <AnimatedButton to="/tours" color="white" animated merriweather>
-          Discover Our Tours
-        </AnimatedButton>
-      </div>
-    </header>
+      <header className="header">
+        <div className="header__logo-box">
+          <Link to="/">
+            <img src="/leafy-travel.png" alt="Logo" className="header__logo" />
+          </Link>
+        </div>
+        <div className="header__text-box">
+          <h1 className="heading__primary">
+            <span className="heading__primary--main">Outdoors</span>
+            <span className="heading__primary--sub">is where life happens</span>
+          </h1>
+          <AnimatedButton to="/tours" color="white" animated merriweather>
+            Discover Our Tours
+          </AnimatedButton>
+        </div>
+      </header>
+    </>
   );
 };
--- client/src/components/navigation/navigation.component.jsx
+++ client/src/components/navigation/navigation.component.jsx
@@ -1,7 +1,50 @@
-import './navigation.styles.scss';
+import "./navigation.styles.scss";
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
   return (
-    <h1>Navigation</h1>
-  )
+    <div className="navigation">
+      <input
+        type="checkbox"
+        className="navigation__checkbox"
+        name="navi-toggle"
+        id="navi-toggle"
+      />
+      <label htmlFor="navi-toggle" className="navigation__button">
+        Menu
+      </label>
+
+      <div className="navigation__background">&nbsp;</div>
+
+      <nav className="navigation__navbar">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <Link to="/about" className="navigation__link">
+              <span>01</span>About Leafy Trails
+            </Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/your-benifits" className="navigation__link">
+              <span>02</span>Your Benifits
+            </Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/tours" className="navigation__link">
+              <span>03</span>Popular Tours
+            </Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/stories" className="navigation__link">
+              <span>04</span>Stories
+            </Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/booknow" className="navigation__link">
+              <span>05</span>Book Now
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
