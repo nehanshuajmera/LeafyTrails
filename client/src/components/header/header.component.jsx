9785acafb5fc339059cb64f0b6fa14d199623f2f
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-21 18:09:46 +0530

    Favicon Changed

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import { AnimatedButton } from "../animated-button/animated-button.component";
 import "./header.styles.scss";
+import { Navigation } from "../navigation/navigation.component";
 
 export const Header = () => {
   return (
     <header className="header">
+      <Navigation />
       <div className="header__logo-box">
         <Link to="/">
           <img src="/leafy-travel.png" alt="Logo" className="header__logo" />
---
+++ client/src/components/navigation/navigation.component.jsx
@@ -0,0 +1,7 @@
+import './navigation.styles.scss';
+
+export const Navigation = () => {
+  return (
+    <h1>Navigation</h1>
+  )
+};
