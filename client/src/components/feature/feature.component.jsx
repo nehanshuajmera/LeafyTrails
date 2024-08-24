4ad9244098d5998e025aff4712ac7e3c6b6f84ad
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-24 11:34:40 +0530

    Feature card converted to take dynamic data

--- client/src/components/feature/feature.component.jsx
+++ client/src/components/feature/feature.component.jsx
@@ -2,30 +2,23 @@ import { GridLayout4x1 } from "../../layout/grid-4x1/grid-layout4x1.component";
 import { FeatureCard } from "../feature-card/feature-card.component";
 import "./feature.styles.scss";
 
+/* Importing Data */
+import { featureData } from "./featureData.js";
+
 export const Feature = () => {
   return (
     <div className="feature">
       <GridLayout4x1>
-        <FeatureCard
-          iconName="basic-world"
-          heading="Explore the world"
-          content="Discover new horizons and create unforgettable memories."
-        />
-        <FeatureCard
-          iconName="basic-compass"
-          heading="Meet Nature"
-          content="Discover Nature: Embrace Beauty, Experience Serenity, Explore."
-        />
-        <FeatureCard
-          iconName="basic-map"
-          heading="Find your way"
-          content="Navigate Life’s Beautiful Journey with Bold Confidence."
-        />
-        <FeatureCard
-          iconName="basic-heart"
-          heading="Live a healthier life"
-          content="Revitalize Your Life: Embrace Health and Happiness!"
-        />
+        {featureData &&
+          featureData
+            .slice(0, 4)
+            .map((feature) => (
+              <FeatureCard
+                iconName={feature.iconName}
+                heading={feature.heading}
+                content={feature.content}
+              />
+            ))}
       </GridLayout4x1>
     </div>
   );
---
+++ client/src/components/feature/featureData.js
@@ -0,0 +1,22 @@
+export const featureData = [
+  {
+    iconName: "basic-world",
+    heading: "Explore the world",
+    content: "Discover new horizons and create unforgettable memories.",
+  },
+  {
+    iconName: "basic-compass",
+    heading: "Meet Nature",
+    content: "Discover Nature: Embrace Beauty, Experience Serenity, Explore.",
+  },
+  {
+    iconName: "basic-map",
+    heading: "Find your way",
+    content: "Navigate Life’s Beautiful Journey with Bold Confidence.",
+  },
+  {
+    iconName: "basic-heart",
+    heading: "Live a healthier life",
+    content: "Revitalize Your Life: Embrace Health and Happiness!",
+  },
+];
