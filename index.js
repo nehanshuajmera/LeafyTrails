a94b58429e9b7d938576fc7c599a6e9296185347
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-26 20:05:49 +0530

    Index file change bug fixes

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import { config } from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 config();
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { storyRoutes } from './routes/storyRoute.js';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { storyRoutes } from "./routes/storyRoute.js";
+
 const PORT = process.env.PORT;
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
-// routes
-app.use('/api/stories', storyRoutes);
+// Routes
+app.use("/api/stories", storyRoutes);
 
 const connect = async () => {
-  await mongoose.connect(process.env.MONGO_DB);
   try {
-    console.log(`Connected to Database`)
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("Connected to Database");
 
     app.listen(PORT, () => {
-      console.log(`App is running on http://localhost:${PORT}`)
+      console.log(`App is running on http://localhost:${PORT}`);
     });
-  }
-  catch {
+  } catch (err) {
     console.error(`Error connecting to the database: ${err.message}`);
     throw new Error(`Error connecting to the database: ${err.message}`);
   }
-}
+};
 
 connect();
 
-// error handling
-app.use = (err, req, res, next) => {
+// Error Handling Middleware
+app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
-};
+});
