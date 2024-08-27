c1c0a037961bd904a92dde939637e8a8e80b7bf0
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-27 23:35:34 +0530

    Stories edited to take dynamic content

--- client/src/components/stories/stories.component.jsx
+++ client/src/components/stories/stories.component.jsx
@@ -1,31 +1,28 @@
 import "./stories.styles.scss";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-// import { fetchStory } from "../../services/storyService.js";
+import { fetchStory } from "../../services/storyService.js";
 import { TestimonialCard } from "../testimonial-card/testimonial-card.component";
 import { BackgroundVideo } from "../background-video/background-video.component";
 
 // images and video import
-import back1Image from "../../assets/story-1.jpg";
-import back2Image from "../../assets/story-2.jpg";
 import backVideo from "../../assets/bg-snow.mp4";
 
 export const Stories = () => {
-//   const [stories, setStories] = useState([]);
+  const [stories, setStories] = useState([]);
 
-//   useEffect(() => {
-//     const getStory = async () => {
-//       try {
-//         const data = await fetchStory();
-//         setStories(data);
-//       }
-//       catch(err){
-//         console.error(`Error fetching Story: ${err.message}`);
-//       }
-//     };
+  useEffect(() => {
+    const getStory = async () => {
+      try {
+        const data = await fetchStory();
+        setStories(data);
+      } catch (err) {
+        console.error(`Error fetching Story: ${err.message}`);
+      }
+    };
 
-//     getStory();
-//   }, [])
+    getStory();
+  }, []);
   return (
     <div className="stories">
       <BackgroundVideo videoMp4={backVideo} />
@@ -33,27 +30,16 @@ export const Stories = () => {
         We make people genuinely happy
       </h2>
       <div className="stories__container">
-
-        {/* {stories && stories.map((story) => (
-          <TestimonialCard 
-            title={story.title}
-            content={story.content}
-            backImage={story.backImage}
-            figCaption={story.figCaption}
-          />
-        ))} */}
-        <TestimonialCard
-          backImage={back1Image}
-          figCaption="Nehanshu Ajmera"
-          title="WOW! my life is completely different now"
-          content="Wow! My life has completely transformed. The adventure we had was beyond anything I could have imagined. Every moment spent in the great outdoors brought a new perspective and an unforgettable sense of peace and fulfillment."
-        />
-        <TestimonialCard
-          backImage={back2Image}
-          figCaption="Naina Mahajan"
-          title="I had best week ever with my family"
-          content="Exploring breathtaking landscapes, we hiked through lush forests, wandered along serene leafy trails, and connected with nature like never before. This unforgettable week with my family was a perfect blend of adventure and tranquility."
-        />
+        {stories &&
+          stories.map((story) => (
+            <TestimonialCard
+              key={story._id}
+              title={story.title}
+              content={story.content}
+              backImage={story.backImage}
+              figCaption={story.figCaption}
+            />
+          ))}
       </div>
       <Link to="/stories" className="btn-text">
         Read all stories&nbsp;
