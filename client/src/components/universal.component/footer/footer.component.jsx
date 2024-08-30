1bc7d6a7ba2d20cf59f21d29210d6a0ba53dccaf
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-08-31 00:33:36 +0530

    New Footer

--- client/src/components/universal.component/footer/footer.component.jsx
+++ client/src/components/universal.component/footer/footer.component.jsx
@@ -1,29 +1,31 @@
 import { Link } from "react-router-dom";
+import { FormInput } from "../form-input/form-input.component";
 import "./footer.styles.scss";
 
-import instagram from "../../../assets/instagram.png";
-import facebook from "../../../assets/facebook.png";
-import twitter from "../../../assets/twitter.png";
-import youtube from "../../../assets/youtube.png";
-import gmail from "../../../assets/gmail.png";
+import instagram from "../../../assets/instagram-1.png";
+import facebook from "../../../assets/facebook-1.png";
+import twitter from "../../../assets/twitter-1.png";
+import youtube from "../../../assets/youtube-1.png";
+import threads from "../../../assets/threads-1.png";
+import arrowRight from "../../../assets/arrow-right.png";
 
 export const Footer = () => {
+  const handleChange = {};
+  const handleSubmit = {};
+
   return (
     <footer className="footer">
-      <div className="footer__tagline">
-        <h3 className="footer__tagline--main">Ready for an Adventure?</h3>
-        <h3 className="footer__tagline--sub">
-          <Link to="/contact">We're Just a Message Away!</Link>
-        </h3>
-      </div>
-      <div className="footer__logo-content">
-        <div className="footer__logo">
-          <Link to="/">
-            <img src="/leafy-travel.png" alt="Footer Logo" />
-          </Link>
+      <div className="footer__container">
+        <div className="footer__container--logo">
+          <span className="logo-design">&nbsp;</span>
+          <span className="heading-box">
+            <h2 className="heading-secondary">Leafy Trails</h2>
+          </span>
+          <span className="logo-design">&nbsp;</span>
         </div>
-        <div className="footer__links">
-          <div className="webpage__links">
+        <div className="footer__container--links">
+          <div className="page-links">
+            <h3 className="heading-tertiary">MENU</h3>
             <div className="page-link">
               <Link to="/">Home</Link>
             </div>
@@ -40,38 +42,78 @@ export const Footer = () => {
               <Link to="/booking">Book Now</Link>
             </div>
           </div>
-          <div className="social-media__links">
-            <div className="media-link instagram">
-              <Link
-                to="https://www.instagram.com/"
-                target="_blank"
-              >
-                <img src={instagram} alt="Instagram" />
-              </Link>
+          <div className="social-links">
+            <div className="social-media">
+              <div className="media-link instagram">
+                <Link to="https://www.instagram.com/" target="_blank">
+                  <img src={instagram} alt="Instagram" />
+                </Link>
+              </div>
+              <div className="media-link facebook">
+                <Link to="https://www.facebook.com/" target="_blank">
+                  <img src={facebook} alt="Facebook" />
+                </Link>
+              </div>
+              <div className="media-link twitter">
+                <Link to="https://x.com/" target="_blank">
+                  <img src={twitter} alt="Twitter" />
+                </Link>
+              </div>
+              <div className="media-link gmail">
+                <Link to="https://www.threads.com/">
+                  <img src={threads} alt="gmail" />
+                </Link>
+              </div>
+              <div className="media-link youtube">
+                <Link to="https://www.youtube.com/" target="_blank">
+                  <img src={youtube} alt="Youtube" />
+                </Link>
+              </div>
             </div>
-            <div className="media-link facebook">
-              <Link to="https://www.facebook.com/" target="_blank">
-                <img src={facebook} alt="Facebook" />
-              </Link>
+            <span className="footer-links-design">&nbsp;</span>
+            <div className="get-in-touch">
+              <div className="footer-main-heading">Ready for an Adventure?</div>
+              <div className="footer-main-tagline">
+                We're Just a Message Away!
+              </div>
+              <form className="footer-form">
+                <FormInput
+                  placeholder="Email"
+                  name="Email"
+                  type="email"
+                  handleChange={handleChange}
+                  // label="Email"
+                  required
+                />
+                <button onSubmit={handleSubmit} className="footer-btn">
+                  <img src={arrowRight} alt="" />
+                  {/* Submit */}
+                </button>
+              </form>
             </div>
-            <div className="media-link twitter">
-              <Link to="https://x.com/" target="_blank">
-                <img src={twitter} alt="Twitter" />
-              </Link>
+          </div>
+          <div className="terms-conditions">
+            <h3 className="heading-tertiary">Terms</h3>
+            <div className="page-link">
+              <Link to="#">Privacy Policy</Link>
             </div>
-            {/* <div className="media-link gmail">
-              <Link to="">
-                <img src={gmail} alt="gmail" />
-              </Link>
-            </div> */}
-            <div className="media-link youtube">
-              <Link
-                to="https://www.youtube.com/"
-                target="_blank"
-              >
-                <img src={youtube} alt="Youtube" />
-              </Link>
+            <div className="page-link">
+              <Link to="#">Terms</Link>
             </div>
+            <div className="page-link">
+              <Link to="#">Cancellation Policy</Link>
+            </div>
+            <div className="page-link">
+              <Link to="#">Copyrights</Link>
+            </div>
+            <div className="page-link">
+              <Link to="#">Fees and Charges</Link>
+            </div>
+          </div>
+        </div>
+        <div className="footer__container--copyright">
+          <div className="copyright-content">
+            &#169; Leafy Trails 2024 | All rights Reserved.
           </div>
         </div>
       </div>
--- client/src/pages/story-page/story-page.component.jsx
+++ client/src/pages/story-page/story-page.component.jsx
@@ -1,11 +1,16 @@
-import './story-page.styles.scss';
+import "./story-page.styles.scss";
 
 export const StoryPage = () => {
   return (
     <div className="story-page">
-      <h2 className="heading-secondary">
-        Story
-      </h2>
+      <div className="story-page__main--heading">
+        <h2 className="heading-secondary">
+          <div>Epic Journeys Shared</div>{" "}
+          <div> Discover Tales from Leafy Trails</div>
+        </h2>
+      </div>
+
+      
     </div>
-  )
-}
+  );
+};
