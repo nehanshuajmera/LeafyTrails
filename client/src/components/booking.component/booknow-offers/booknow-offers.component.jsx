d547db7044d54f2bdcb1d81c74bb978093cd08e4
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-09-03 23:48:43 +0530

    Copy to clipboard feature updated to occur with popup

--- client/src/components/booking.component/booknow-offers/booknow-offers.component.jsx
+++ client/src/components/booking.component/booknow-offers/booknow-offers.component.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./booknow-offers.styles.scss";
 import copy from "../../../assets/copy.png";
 
 export const BooknowOffers = () => {
-  /* Function to copy coupon code */ 
+  const [isCopied, setIsCopied] = useState(false);
+
+  /* Function to copy coupon code and show popup */
   const copyToClipboard = () => {
-    const couponCode = "FamilyFun30";
+    const couponCode = "FunFamily30";
     navigator.clipboard.writeText(couponCode).then(
       () => {
-        alert("Coupon code copied to clipboard!");
+        setIsCopied(true); /* Show popup */
+        setTimeout(
+          () => setIsCopied(false),
+          3000
+        ); /* Hide popup after 3 seconds */
       },
       () => {
         alert("Failed to copy the coupon code.");
@@ -23,7 +29,7 @@ export const BooknowOffers = () => {
           <div className="offer--name">Limited Time Offer</div>
           <div className="offer--title">Family Fun, Discounted Fun!</div>
           <div className="offer--content">
-            Book Your Family Adventure Today and Enjoy 30% Off All Tours Create
+            Book Your Family Adventure Today and Enjoy 30% Off All Tours. Create
             unforgettable memories with your loved ones. Explore breathtaking
             destinations, experience thrilling adventures, and discover new
             cultures together. Our family tours are designed to cater to all
@@ -36,13 +42,17 @@ export const BooknowOffers = () => {
           <div className="offer--coupon-code">
             <span className="coupon-text">Use Coupon Code:</span>
             <span className="coupon-code">
-              FamilyFun30 &nbsp;
+              FunFamily30 &nbsp;
               <span className="copy-coupon" onClick={copyToClipboard}>
                 <img src={copy} alt="copy icon" />
               </span>
             </span>
           </div>
         </div>
+        {/* Popup Notification */}
+        {isCopied && (
+          <div className="copy-popup">Coupon code copied to clipboard!</div>
+        )}
       </div>
     </div>
   );
