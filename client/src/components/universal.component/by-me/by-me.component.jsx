716ba1e53e90f5e8edf9073a1cb680818734374b
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-09-06 03:34:16 +0530

    Happy Coding, By me updated, popup made responsive and made website responsive to all smaller devices than 1280px, Done for now - untill next time

--- client/src/components/universal.component/by-me/by-me.component.jsx
+++ client/src/components/universal.component/by-me/by-me.component.jsx
@@ -1,9 +1,23 @@
 import "./by-me.styles.scss";
+import { Link } from "react-router-dom";
 
 export const ByNehanshu = () => {
   return (
     <div className="by-nehanshu">
-      <h2 className="heading-secondary">Presenting Leafy Trails | By Nehanshu Ajmera</h2>
+      <span className="my-footer--1">
+        Presenting{" "}
+        <Link to="https://leafytrails.netlify.app/" target="_blank">
+          {" "}
+          Leafy Trails{" "}
+        </Link>{" "}
+        By&nbsp;
+      </span>
+      <h2 className="heading-secondary">
+        <Link to="https://github.com/nehanshuajmera" target="_blank">
+          {" "}
+          Nehanshu Ajmera{" "}
+        </Link>
+      </h2>
     </div>
   );
 };
